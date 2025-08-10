0e43ce9a868178af00e2afdd133d833012c76bb6
Sandeep Gour <sandeepgour597@gmail.com>
2025-08-10 15:28:54 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -15,7 +15,7 @@ function searchStudent() {
     return;
   }
 
-  fetch("http://localhost:8081/api/students/search/" + input, {
+  fetch("https://schoolfeemanagementapp-production.up.railway.app/api/students/search/" + input, {
     method: "GET",
     headers: {
       Authorization: "Bearer " + token, // ✅ token added
