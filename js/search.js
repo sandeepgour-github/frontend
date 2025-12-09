805c4e106116789cab3cac315e52a904442a3f93
Sandeep Gour <sandeepgour597@gmail.com>
2025-12-10 00:32:57 +0530

    Change API endpoint to production server

--- js/search.js
+++ js/search.js
@@ -15,7 +15,7 @@ function searchStudent() {
     return;
   }
 
-  fetch("http://localhost:8082/api/students/search/" + input, {
+  fetch("https://schoolfeemanagementapp-production.up.railway.app/api/students/search/" + input, {
     method: "GET",
     headers: {
       Authorization: "Bearer " + token, // token added
